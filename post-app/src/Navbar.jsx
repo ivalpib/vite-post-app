89f813553febca4f95bc8242e98cd3a14b9251df
ivalpib <valpib3@gmail.com>
2024-06-09 12:51:46 +1000

    creating user manual

---
+++ post-app/src/Navbar.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+function Navbar(){
+    return (
+        <>     
+            <ul>
+                <a href=''><li>Post</li></a>
+                <a href=''><li>Profile</li></a>
+                <a href=''><li>Contact</li></a>
+                <a href=''><li>About</li></a>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar
--- post-app/src/main.jsx
+++ post-app/src/main.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greet from './Greet.jsx'
+import Navbar from './Navbar.jsx'
 
+ReactDOM.createRoot(document.getElementById('nav')).render(
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>,
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
